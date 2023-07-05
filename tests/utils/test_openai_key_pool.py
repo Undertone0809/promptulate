7414e6c86d8a61a73339eb3bfa5da227ad786540
Zeeland <zeeland@foxmail.com>
2023-07-05 23:40:16 +0800

    pref: optimize key pool

--- tests/utils/test_openai_key_pool.py
+++ tests/utils/test_openai_key_pool.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from promptulate.utils.openai_key_pool import (
+    OpenAIKey,
     OpenAIKeyPool,
     export_openai_key_pool,
     add_key_to_key_pool,
@@ -8,23 +9,30 @@ from promptulate.utils.openai_key_pool import (
 
 
 class TestOpenAIKeyPool(TestCase):
+    key_pool: OpenAIKeyPool = None
+
     @classmethod
     def setUpClass(cls):
         cls.key_pool: OpenAIKeyPool = OpenAIKeyPool()
+        cls.key_pool.cache.remove_duplicates(OpenAIKey)
 
     def test_set_key_pool(self):
         keys = [
             {"model": "gpt-3.5-turbo", "key": "key1"},
             {"model": "gpt-3.5-turbo", "key": "key2"},
             {"model": "gpt-3.5-turbo", "key": "key3"},
+            {"model": "gpt-3.5-turbo", "keys": "key4,key5,key6"},
             {"model": "gpt-4", "key": "key4"},
         ]
         self.key_pool.set(keys)
         num = self.key_pool.get_num("gpt-3.5-turbo")
-        self.assertEqual(num, 3)
+        self.assertEqual(num, 6)
         self.assertEqual(self.key_pool.get("gpt-3.5-turbo"), "key1")
         self.assertEqual(self.key_pool.get("gpt-3.5-turbo"), "key2")
         self.assertEqual(self.key_pool.get("gpt-3.5-turbo"), "key3")
+        self.assertEqual(self.key_pool.get("gpt-3.5-turbo"), "key4")
+        self.assertEqual(self.key_pool.get("gpt-3.5-turbo"), "key5")
+        self.assertEqual(self.key_pool.get("gpt-3.5-turbo"), "key6")
         self.assertEqual(self.key_pool.get("gpt-3.5-turbo"), "key1")
         self.assertEqual(self.key_pool.get("gpt-4"), "key4")
 
@@ -46,11 +54,12 @@ class TestOpenAIKeyPool(TestCase):
 
     def test_append_key_pool(self):
         keys = [
-            {"model": "gpt-3.5-turbo", "key": "key4"},
-            {"model": "gpt-3.5-turbo", "key": "key5"},
-            {"model": "gpt-3.5-turbo", "key": "key6"},
-            {"model": "gpt-4", "key": "key7"},
+            {"model": "gpt-3.5-turbo", "key": "key7"},
+            {"model": "gpt-3.5-turbo", "key": "key8"},
+            {"model": "gpt-3.5-turbo", "key": "key9"},
+            {"model": "gpt-3.5-turbo", "keys": "key10,key11,key12"},
+            {"model": "gpt-4", "key": "key10"},
         ]
         add_key_to_key_pool(keys)
         num = self.key_pool.get_num("gpt-3.5-turbo")
-        self.assertEqual(num, 6)
+        self.assertEqual(num, 12)
