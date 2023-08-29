255aceff8abd5a4bedeef07a60f66b8953357865
Zeeland <zeeland@foxmail.com>
2023-08-29 22:04:03 +0800

    optimize sleep tool

--- promptulate/tools/sleep/tool.py
+++ promptulate/tools/sleep/tool.py
@@ -1,3 +1,4 @@
+from typing import Union
 from time import sleep
 
 from promptulate.tools import BaseTool
@@ -5,11 +6,14 @@ from promptulate.tools import BaseTool
 
 class SleepTool(BaseTool):
     name: str = "sleep"
-    description: str = "Make agent sleep for a specified number of seconds."
+    description: str = (
+        "Make agent sleep for a specified number of seconds."
+        "Input is a number. eg: Sleep for 5s and enter 5"
+    )
 
     def _run(
         self,
-        sleep_time,
+        sleep_time: Union[int, float],
     ) -> str:
         """Use the Sleep tool."""
         sleep(int(sleep_time))
