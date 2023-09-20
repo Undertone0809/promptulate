17ac5840689e0cb4f85edffd0320163aad178aae
Zeeland <zeeland@foxmail.com>
2023-09-20 23:41:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setuptools.setup(
     name="promptulate",
-    version="1.7.0",
+    version="1.7.1",
     author="Zeeland",
     author_email="zeeland@foxmail.com",
     description="A powerful LLM Application development framework.",
