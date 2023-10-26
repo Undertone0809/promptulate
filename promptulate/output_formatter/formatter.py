52d70319afca17e4545b2834ba88b69eb7f72e72
Zeeland <zeeland@foxmail.com>
2023-10-26 17:55:54 +0800

    feat: add examples for OutputFormatter

--- promptulate/output_formatter/formatter.py
+++ promptulate/output_formatter/formatter.py
@@ -1,6 +1,6 @@
 import json
 import re
-from typing import TypeVar
+from typing import TypeVar, List, Dict
 
 from pydantic import BaseModel
 
@@ -10,38 +10,58 @@ from promptulate.tips import OutputParserError
 T = TypeVar("T", bound=BaseModel)
 
 
+def _get_schema(pydantic_obj: type(BaseModel)) -> Dict:
+    """Get reduced schema from pydantic object."""
+
+    # Remove useless fields.
+    reduced_schema = pydantic_obj.schema()
+    if "title" in reduced_schema:
+        del reduced_schema["title"]
+    if "type" in reduced_schema:
+        del reduced_schema["type"]
+    for k, v in reduced_schema["properties"].items():
+        if "title" in v:
+            del v["title"]
+
+    return reduced_schema
+
+
 class OutputFormatter:
-    def __init__(self, pydantic_obj: type(BaseModel)):
+    def __init__(self, pydantic_obj: type(BaseModel), examples: List[BaseModel] = None):
         self.pydantic_obj = pydantic_obj
+        self.examples = examples
 
     def get_formatted_instructions(self) -> str:
-        schema = self.pydantic_obj.schema()
-
-        # Remove useless fields.
-        reduced_schema = schema
-        if "title" in reduced_schema:
-            del reduced_schema["title"]
-        if "type" in reduced_schema:
-            del reduced_schema["type"]
-        for k, v in reduced_schema["properties"].items():
-            if "title" in v:
-                del v["title"]
-
-        # Ensure json with double quotes.
-        schema_str = json.dumps(reduced_schema)
-
-        return OUTPUT_FORMAT.format(schema=schema_str)
+        return get_formatted_instructions(self.pydantic_obj, self.examples)
 
     def formatting_result(self, llm_output: str) -> T:
-        """Parse llm_output and instantiate the result as provided pydantic_obj."""
-        try:
-            match = re.search(
-                r"\{.*\}", llm_output.strip(), re.MULTILINE | re.IGNORECASE | re.DOTALL
-            )
-            json_str = match.group() if match else ""
-            json_object = json.loads(json_str, strict=False)
-            return self.pydantic_obj.parse_obj(json_object)
-        except Exception as e:
-            name = self.pydantic_obj.__name__
-            msg = f"Failed to parse {name} from completion {llm_output}. Got: {e}"
-            raise OutputParserError(msg, llm_output)
+        return formatting_result(self.pydantic_obj, llm_output)
+
+def get_formatted_instructions(
+    pydantic_obj: type(BaseModel), examples: List[BaseModel] = None
+) -> str:
+    # Ensure json with double quotes.
+    schema_str = json.dumps(_get_schema(pydantic_obj))
+
+    instructions: str = OUTPUT_FORMAT.format(schema=schema_str)
+    if examples:
+        instructions += "\nExamples:\n"
+        for example in examples:
+            example_str = json.dumps(example.dict())
+            instructions += f"{example_str}\n"
+    return instructions
+
+
+def formatting_result(pydantic_obj: type(BaseModel), llm_output: str) -> T:
+    """Parse llm_output and instantiate the result as provided pydantic_obj."""
+    try:
+        match = re.search(
+            r"\{.*\}", llm_output.strip(), re.MULTILINE | re.IGNORECASE | re.DOTALL
+        )
+        json_str = match.group() if match else ""
+        json_object = json.loads(json_str, strict=False)
+        return pydantic_obj.parse_obj(json_object)
+    except Exception as e:
+        name = pydantic_obj.__name__
+        msg = f"Failed to parse {name} from completion {llm_output}. Got: {e}"
+        raise OutputParserError(msg, llm_output)
