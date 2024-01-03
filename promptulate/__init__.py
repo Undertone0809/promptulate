ef861aa3971d4978f5e1cb716a4651e731b0b200
zeeland <zeeland4work@gmail.com>
2024-01-04 03:55:18 +0800

    pref: optimize pne import

--- promptulate/__init__.py
+++ promptulate/__init__.py
@@ -34,11 +34,13 @@ from promptulate.schema import (
     UserMessage,
 )
 from promptulate.tools.base import BaseTool, Tool, define_tool
-from promptulate.utils import enable_log
+from promptulate.utils.logger import enable_log
+from promptulate.utils.string_template import StringTemplate
 
 _util_fields = [
     "enable_log",
     "OutputFormatter",
+    "StringTemplate",
 ]
 
 _schema_fields = [
