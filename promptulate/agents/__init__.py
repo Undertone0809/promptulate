37cd145379734d7cd271a3c204c82a10a7d9a8a1
zeeland <zeeland4work@gmail.com>
2024-01-08 22:05:11 +0800

    pref: optimize module import

--- promptulate/agents/__init__.py
+++ promptulate/agents/__init__.py
@@ -1,3 +1,11 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from promptulate.agents.base import BaseAgent
+    from promptulate.agents.tool_agent.agent import ToolAgent
+    from promptulate.agents.web_agent.agent import WebAgent
+
+
 def __getattr__(name):
     if name == "ToolAgent":
         from promptulate.agents.tool_agent.agent import ToolAgent
