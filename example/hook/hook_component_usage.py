899e6569740b3ac41b37aa0b1db87a939a3b4607
Hizeros <3290606026@qq.com>
2024-05-04 01:55:47 +0800

    style:Make format of code

--- example/hook/hook_component_usage.py
+++ example/hook/hook_component_usage.py
@@ -13,10 +13,11 @@ def handle_tool_create_by_component(*args, **kwargs):
 def main():
     # handle tool create by component
     llm = ChatOpenAI(model="gpt-4-1106-preview")
-    agent = pne.ToolAgent(tools=[wikipedia_search, calculator],
-                          llm=llm)
-    response: str = agent.run("Tell me the year in which Tom Cruise's Top Gun was "
-                              "released, and calculate the square of that year.")
+    agent = pne.ToolAgent(tools=[wikipedia_search, calculator], llm=llm)
+    response: str = agent.run(
+        "Tell me the year in which Tom Cruise's Top Gun was "
+        "released, and calculate the square of that year."
+    )
     print(response)
 
 
