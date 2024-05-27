8f2d6eafd7d211838aa87abfcc23c0cf5fc31bda
Hizeros <3290606026@qq.com>
2024-05-27 13:16:46 +0800

    feat:Add streamlit+pne.chat to build use case of simple chatbot

---
+++ example/streamlit-chatbot/README.md
@@ -0,0 +1,29 @@
+# Build a simple chatbot using streamlit and pne
+
+The `app.py` is an example of building a simple chatbot using streamlit and promptulate.
+
+# Quick Start
+
+1. Clone the repository and install the dependencies
+
+```shell
+git clone https://www.github.com/Undertone0809/promptulate
+```
+
+2. Switch the current directory to the example
+
+```shell
+cd ./example/streamlit-chatbot
+```
+
+3. Install the dependencies
+
+```shell
+pip install -r requirements.txt
+```
+
+4. Run the application
+
+```shell
+streamlit run app.py
+```
---
+++ example/streamlit-chatbot/app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+
+import promptulate as pne
+
+# Create a sidebar to place the user parameter configuration
+with st.sidebar:
+    model_name: str = st.text_input(
+        label="LLM Model Name",
+        help="1.gpt-4-1106-preview "
+        "2.deepseek/deepseek-chat "
+        "For more details, please click ("
+        "https://www.promptulate.cn/#/use_cases/chat_usage?id=chat)",
+    )
+    api_key = st.text_input("API Key", key="chatbot_api_key", type="password")
+    api_base = st.text_input("OpenAI Proxy URL (Optional)")
+
+# Set title
+st.title("💬 Chat")
+st.caption("🚀 Hi there! 👋 I am a simple chatbot by Promptulate to help you.")
+
+# Determine whether to initialize the message variable
+# otherwise initialize a message dictionary
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "How can I help you?"}
+    ]
+
+# Traverse messages in session state
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+# User input
+if prompt := st.chat_input():
+    if not api_key:
+        st.info("Please add your API key to continue.")
+        st.stop()
+
+    # Add the message entered by the user to the list of messages in the session state
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display in the chat interface
+    st.chat_message("user").write(prompt)
+
+    response: str = pne.chat(
+        model=model_name,
+        stream=True,
+        messages=prompt,
+        model_config={"api_base": api_base, "api_key": api_key},
+    )
+
+    for i in response:
+        st.session_state.messages.append({"role": "assistant", "content": i})
+        st.chat_message("assistant").write(i)
