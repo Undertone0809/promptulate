b8c5fcb018114439c0119847033b8b8c05ccdd45
luffy <1527211162@qq.com>
2024-09-17 23:51:14 +0800

    add wx-chatbot Use Case

---
+++ example/wx-chatbot/app.py
@@ -0,0 +1,88 @@
+import time
+
+import chat_message
+import itchat
+import pne
+from itchat.content import TEXT
+from itchat.storage.messagequeue import Message
+
+from promptulate.utils import logger
+
+
+@itchat.msg_register([TEXT])
+def handler_single_msg(msg: Message):
+    try:
+        print(msg)
+        print("Get a new messsage: {}".format(msg.Content))
+        handler.handle(chat_message.ReceiveMessage(msg))
+    except NotImplementedError as e:
+        logger.debug("[WX]single message {} skipped: {}".format(msg["MsgId"], e))
+        return None
+    return None
+
+
+def qrCallback(uuid, status, qrcode):
+    # logger.debug("qrCallback: {} {}".format(uuid,status))
+    if status == "0":
+        url = f"https://login.weixin.qq.com/l/{uuid}"
+
+        qr_api1 = "https://api.isoyu.com/qr/?m=1&e=L&p=20&url={}".format(url)
+        qr_api2 = (
+            "https://api.qrserver.com/v1/create-qr-code/?size=400×400&data={}".format(
+                url
+            )
+        )
+        qr_api3 = "https://api.pwmqr.com/qrcode/create/?url={}".format(url)
+        qr_api4 = "https://my.tv.sohu.com/user/a/wvideo/getQRCode.do?text={}".format(
+            url
+        )
+        print("You can also scan QRCode in any website below:")
+        print(qr_api3)
+        print(qr_api4)
+        print(qr_api2)
+        print(qr_api1)
+
+
+def startup():
+    try:
+        itchat.instance.receivingRetryCount = 600  # 修改断线超时时间
+        hotReload = False
+        itchat.auto_login(
+            enableCmdQR=2,
+            hotReload=hotReload,
+            qrCallback=qrCallback,
+        )
+        user_id = itchat.instance.storageClass.userName
+        name = itchat.instance.storageClass.nickName
+        logger.info(
+            "Wechat login success, user_id: {}, nickname: {}".format(user_id, name)
+        )  # noqa: E501
+        itchat.run()
+    except Exception as e:
+        logger.exception(e)
+
+
+class MessageHandler:
+    def __init__(self):
+        pass
+
+    def handle(self, msg: chat_message.ReceiveMessage):
+        receiver = msg.FromUserName
+        response = pne.chat(
+            messages=msg.Content,
+            model="gpt-3.5-turbo",
+            model_config={
+                "api_key": "sk-xxxxxx",
+                "base_url": "https://api.openai.com/v1",
+            },
+        )
+        itchat.send(response.result, toUserName=receiver)
+
+
+handler = MessageHandler()
+
+
+if __name__ == "__main__":
+    startup()
+    while True:
+        time.sleep(1)
---
+++ example/wx-chatbot/itchat/returnvalues.py
@@ -0,0 +1,78 @@
+# coding=utf8
+TRANSLATE = "Chinese"
+
+
+class ReturnValue(dict):
+    """turn return value of itchat into a boolean value
+    for requests:
+        ..code::python
+
+            import requests
+            r = requests.get('http://httpbin.org/get')
+            print(ReturnValue(rawResponse=r)
+
+    for normal dict:
+        ..code::python
+
+            returnDict = {
+                'BaseResponse': {
+                    'Ret': 0,
+                    'ErrMsg': 'My error msg', }, }
+            print(ReturnValue(returnDict))
+    """
+
+    def __init__(self, returnValueDict={}, rawResponse=None):
+        if rawResponse:
+            try:
+                returnValueDict = rawResponse.json()
+            except ValueError:
+                returnValueDict = {
+                    "BaseResponse": {
+                        "Ret": -1004,
+                        "ErrMsg": "Unexpected return value",
+                    },
+                    "Data": rawResponse.content,
+                }
+        for k, v in returnValueDict.items():
+            self[k] = v
+        if "BaseResponse" not in self:
+            self["BaseResponse"] = {
+                "ErrMsg": "no BaseResponse in raw response",
+                "Ret": -1000,
+            }
+        if TRANSLATE:
+            self["BaseResponse"]["RawMsg"] = self["BaseResponse"].get("ErrMsg", "")
+            self["BaseResponse"]["ErrMsg"] = TRANSLATION[TRANSLATE].get(
+                self["BaseResponse"].get("Ret", "")
+            ) or self["BaseResponse"].get("ErrMsg", "No ErrMsg")
+            self["BaseResponse"]["RawMsg"] = (
+                self["BaseResponse"]["RawMsg"] or self["BaseResponse"]["ErrMsg"]
+            )
+
+    def __nonzero__(self):
+        return self["BaseResponse"].get("Ret") == 0
+
+    def __bool__(self):
+        return self.__nonzero__()
+
+    def __str__(self):
+        return "{%s}" % ", ".join(
+            ["%s: %s" % (repr(k), repr(v)) for k, v in self.items()]
+        )
+
+    def __repr__(self):
+        return "<ItchatReturnValue: %s>" % self.__str__()
+
+
+TRANSLATION = {
+    "Chinese": {
+        -1000: "返回值不带BaseResponse",
+        -1001: "无法找到对应的成员",
+        -1002: "文件位置错误",
+        -1003: "服务器拒绝连接",
+        -1004: "服务器返回异常值",
+        -1005: "参数错误",
+        -1006: "无效操作",
+        0: "请求成功",
+    },
+}
